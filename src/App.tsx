b717d08100f86c38e4a70c4150b4b2daf9647658
Daniel Nelson <danieljay.vh@gmail.com>
2026-02-03 10:58:50 +1300

    structure and layout of main page

--- src/App.tsx
+++ src/App.tsx
@@ -16,21 +16,55 @@ function App() {
   }, [message]);
 
 
+
   return (
-    <div style={{ width: '100%', height: '100%', justifyContent: 'center', display: 'flex', flexDirection: 'row'}} >
-      <nav style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-around', width: '100%' }}>{/*nav*/}
-        <div>
-          <p onClick={() => setpage("tasks")} >Tasks</p>
+    <div style={{ width: '90vw', height: '90vh', justifyContent: 'space-between', display: 'flex', flexDirection: 'row' }} >
+      <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', height: '100%' }}>
+        <nav style={{ display: 'flex', flexDirection: 'column', alignContent: 'space-between' }}>{/*nav*/}
+          <div>
+            <p onClick={() => setpage("tasks")} >Tasks</p>
+          </div>
+          <div>
+            <p onClick={() => setpage("now")} title='now'>Now</p>
+          </div>
+          <div>
+            <p onClick={() => setpage("settings")} >Settings</p>
+          </div>
+        </nav>
+        <div className='meta' style={{ display: 'flex', flexDirection: 'column' }} > {/*TODO: add meta class to modular styles*/}
+          <h6>
+            meta
+          </h6>
+          <table>
+            <th></th>
+
+            <tr><td>
+              <p style={{ fontSize: '8px' }}> time: </p>
+            </td>
+              <td>
+              <p style={{ fontSize: '8px' }}> {new Date().toLocaleTimeString()} </p>
+              </td>
+            </tr>
+            <tr><td>
+              <p style={{ fontSize: '8px' }}> version </p>
+            </td> </tr>
+            <tr><td>
+              <p style={{ fontSize: '8px' }}> update date </p>
+            </td> </tr>
+            <tr><td>
+              <p style={{ fontSize: '8px' }}> location: </p>
+            </td> </tr>
+            <tr><td>
+              <p style={{ fontSize: '8px' }}> last active: </p>
+            </td> </tr>
+            <tr><td>
+              <p style={{ fontSize: '8px' }}> copyright </p>
+            </td> </tr>
+          </table>
         </div>
-        <div>
-          <p onClick={() => setpage("now")} title='now'>Now</p>
-        </div>
-        <div>
-          <p onClick={() => setpage("settings")} >Settings</p>
-        </div>
-      </nav>
+      </div>
       <div> {/*main container*/}
-        <h1>message = "{message}"</h1>
+        <h1>connected to backend = "{message}"</h1>
         {page == 'now' && (
           <Now />)}
         {page == 'tasks' && (
@@ -39,8 +73,13 @@ function App() {
           <Settings />)}
         <h2 >message= quote of the day</h2>
       </div>
-      <div> {/*Live view and habits*/}
-hello
+      <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}> {/*Live view and habits*/}
+        <div>
+          live view
+        </div>
+        <div>
+          habits
+        </div>
       </div>
 
     </div>
