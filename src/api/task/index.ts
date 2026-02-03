7a30d6e8f004e2d7685abc2f6028ec11e2227d3b
Daniel Nelson <danieljay.vh@gmail.com>
2026-02-04 01:09:17 +1300

    update url for backend

--- src/api/task/index.ts
+++ src/api/task/index.ts
@@ -1,12 +1,13 @@
 import type { task } from "../../types/task";
 
 export function createTask(task: Partial<task>) {
-  fetch('https://localhost:443/api/task',{
-      method: "POST", // Specify the method
+  console.log(task)
+  fetch('http://0.0.0.0:3002/task', {
+    method: "POST", // Specify the method
     headers: {
-    "Content-Type": "application/json", // Tell server you're sending JSON
-  },
+      "Content-Type": "application/json", // Tell server you're sending JSON
+    },
     body: JSON.stringify(task), // Body must be a string for JSON
-    });
-  
+  });
+
 }
