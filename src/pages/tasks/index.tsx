dafca862e761b09120fa94db3a372c036ab71630
Daniel Nelson <danieljay.vh@gmail.com>
2026-02-05 22:24:33 +1300

    add more task fields
    ran first successful api post statement, created a task.

--- src/pages/tasks/index.tsx
+++ src/pages/tasks/index.tsx
@@ -1,33 +1,52 @@
 import { useState, type ChangeEvent } from "react";
 import { CreateTaskButton } from "../../components/buttons/createTaskButton";
 import { createTask } from "../../api/task";
+import type { task } from "../../types/task";
 
 type frequency = "never" | "daily" | "weekly" | "monthly | yearly"
 export function Tasks() {
 
   const [name, setName] = useState("")
-  const [startTime, setStartTime] = useState(new Date())
-  const [endTime, setEndTime] = useState(new Date())
+  const [startTime, setStartTime] = useState("")
+  const [endTime, setEndTime] = useState("")
   const [location, setLocation] = useState("TBC")
   const [priority, setPriority] = useState(1)
   const [repeatingFrequency, setRepeatingFrequency] = useState<frequency>("never")
   const [repeatingFrequencyCount, setRepeatingFrequencyCount] = useState(1)
-  const [date, setDate] = useState(new Date())
+  const [date, setDate] = useState("")
 
-  const handleNameChange = (event: ChangeEvent<HTMLInputElement>) => {
-    if (!event.target || !event.target.value) return
-    setName(event.target.value);
-  };
+
   const handleCreateButtonClick = () => {
-    createTask({ name, startTime, endTime, date })
+    createTask({ name:name, startTime:startTime, endTime:endTime, date:date, priority:priority } as task)
   }
+
   return (
     <>
       <p>Task page</p>
       <div style={{ display: "flex", flexDirection: 'column' }}>
+                <input placeholder="Do the thing!" onChange={(e) => setName(e.target.value)} />
 
-        <input placeholder="Do the thing!" onChange={handleNameChange} />
         <p>{name}</p>
+      </div>
+
+      <div style={{ display: "flex", flexDirection: 'column' }}>
+        <input placeholder="5" type='number' onChange={(e) => setPriority(Number(e.target.value))} />
+        <p>{priority}</p>
+      </div>
+
+      <div style={{ display: "flex", flexDirection: 'column' }}>
+        <input placeholder="Do the thing!" type='date' onChange={(e) => setDate((e.target.value))} />
+        <p>{date.toString()}</p>
+      </div>
+
+      <div style={{ display: "flex", flexDirection: 'column' }}>
+        <input placeholder="Do the thing!" type='date' onChange={(e) => setStartTime((e.target.value))} />
+        <p>{startTime}</p>
+      </div>
+
+      <div style={{ display: "flex", flexDirection: 'column' }}>
+        <input placeholder="Do the thing!" type='date' onChange={(e) => setEndTime((e.target.value))} />
+        <p>{endTime}</p>
       </div>
       <CreateTaskButton onClick={handleCreateButtonClick} />
     </>
